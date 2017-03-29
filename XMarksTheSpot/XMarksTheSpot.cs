c5aa0a9e5e0e79ceff49616e8ef28868307a6120
kopets <kopets@i.ua>
2017-03-29 13:36:24 +0300

    XMarksTheSpot program added.

---
+++ XMarksTheSpot/XMarksTheSpot.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+class XMarksTheSpot : Form
+{
+    static void Main()
+    {
+        Application.Run(new XMarksTheSpot());
+    }
+
+    public XMarksTheSpot()
+    {
+        Text = "X Marks The Spot";
+        BackColor = SystemColors.Window;
+        ForeColor = SystemColors.WindowText;
+        ResizeRedraw = true;
+    }
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        //base.OnPaint(e);
+        Graphics grfx = e.Graphics;
+        Pen pen = new Pen(ForeColor);
+
+        grfx.DrawLine(pen, 0, 0, ClientSize.Width - 1, ClientSize.Height - 1);
+        grfx.DrawLine(pen, 0, ClientSize.Height - 1, ClientSize.Width - 1, 0);
+    }
+
+}
