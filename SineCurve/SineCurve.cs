8b9e69251d1c1d605e99761272eb9680a9b3ff63
kopets <kopets@i.ua>
2017-04-06 21:00:55 +0300

    added SineCurve

---
+++ SineCurve/SineCurve.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+class SineCurve : PrintableForm
+{
+    public static new void Main()
+    {
+        Application.Run(new SineCurve());
+    }
+
+    public SineCurve()
+    {
+        Text = "Sine Curve";
+    }
+
+    protected override void DoPage(Graphics grfx, Color clr, int cx, int cy)
+    {
+        // base.DoPage(grfx, clr, cx, cy);
+        PointF[] aptf = new PointF[cx];
+
+        for (int i = 0; i < cx; i++)
+        {
+            aptf[i].X = i;
+            aptf[i].Y = cy/2 * (1 - (float)Math.Sin(i*2*Math.PI/(cx-1)));
+        }
+
+        grfx.DrawLines(new Pen(clr), aptf);
+    }
+}
