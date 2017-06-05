c679d92fc66a9f2347739667ba1133bbf78da4a3
kingura <kopets@i.ua>
2017-06-05 08:23:30 +0300

    added PrintableTenCentimeterRuler, TenCentimeterRulerAuto, PenWidths, SixInchRuler.

---
+++ PenWidths/PenWidths.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+class PenWidths : PrintableForm
+{
+    static public new void Main()
+    {
+        Application.Run(new PenWidths());
+    }
+
+    public PenWidths()
+    {
+        Text = "Pen Widths";
+    }
+
+    protected override void DoPage(Graphics grfx, Color clr, int cx, int cy)
+    {
+        Brush brush = new SolidBrush(clr);
+        float y = 0;
+
+        for (float f = 0; f < 3.2f; f += 0.2f)
+        {
+            Pen pen = new Pen(clr, f);
+            string str = String.Format("{0:F1}-point-wide pen: ", f);
+            SizeF sizef = grfx.MeasureString(str, Font);
+
+            grfx.DrawString(str, Font, brush, 0, y);
+            grfx.DrawLine(pen, sizef.Width, y + sizef.Height/2, sizef.Width + 144, y + sizef.Height/2);
+
+            y += sizef.Height;
+        }
+    }
+}
---
+++ SixInchRuler/SixInchRuler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+class SixInchRuler : PrintableForm
+{
+    public new static void Main()
+    {
+        Application.Run(new SixInchRuler());
+    }
+    public SixInchRuler()
+    {
+        Text = "Six-Inch Ruler";
+    }
+    protected override void DoPage(Graphics grfx, Color clr, int cx, int cy)
+    {
+        Brush brush = new SolidBrush(clr);
+        Pen pen = new Pen(clr, 0.5f);
+        const int xOffset = 16;
+        const int yOffset = 16;
+
+        grfx.PageUnit = GraphicsUnit.Inch;
+        grfx.PageScale = 1 / 64f;
+        grfx.DrawRectangle(pen, xOffset, yOffset, 6 * 64, 64);
+
+        StringFormat strfmt = new StringFormat();
+        strfmt.Alignment = StringAlignment.Center;
+
+        for (int i = 1; i < 6 * 16; i++)
+        {
+            int x = xOffset + i * 4;
+            int y = yOffset;
+            int dy;
+
+            if (i % 16 == 0)         // Дюймовые деления
+            {
+                dy = 32;
+                grfx.DrawString((i / 16).ToString(), Font, brush,
+                                              x, y + dy, strfmt);
+            }
+            else if (i % 8 == 0)     // Полудюймовые деления
+                dy = 24;
+
+            else if (i % 4 == 0)     // Деления для 1/4 дюйма
+                dy = 20;
+
+            else if (i % 2 == 0)     // Деления для 1/8 дюйма
+                dy = 16;
+
+            else                     // Деления для 1/16 дюйма
+                dy = 12;
+
+            grfx.DrawLine(pen, x, y, x, y + dy);
+        }
+    }
+}
