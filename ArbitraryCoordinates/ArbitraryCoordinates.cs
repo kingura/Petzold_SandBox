fba72ca2198ba0fb11fbea2e25ab2edc9b0494b3
kopets <kopets@i.ua>
2017-06-09 14:50:41 +0300

    added ArbitraryCoordinates

---
+++ ArbitraryCoordinates/ArbitraryCoordinates.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+class ArbitraryCoordinates: PrintableForm
+{
+    static public new void Main()
+    {
+        Application.Run(new ArbitraryCoordinates());
+    }
+
+    public ArbitraryCoordinates()
+    {
+        Text = "Arbitrary Coordinates";
+        MinimumSize = SystemInformation.MinimumWindowSize + new Size(0, 1);
+    }
+
+    protected override void DoPage(Graphics grfx, Color clr, int cx, int cy)
+    {
+        grfx.PageUnit = GraphicsUnit.Pixel;
+        SizeF sizef = grfx.VisibleClipBounds.Size;
+
+        grfx.PageUnit = GraphicsUnit.Inch;
+        //try
+        //{
+            grfx.PageScale = Math.Min(sizef.Width / grfx.DpiX / 1000,
+                                      sizef.Height / grfx.DpiX / 1000);
+        //}
+        //catch
+        //{
+        //    return;
+        //}
+
+        grfx.DrawEllipse(new Pen(clr), 0, 0, 990, 990);
+    }
+}
