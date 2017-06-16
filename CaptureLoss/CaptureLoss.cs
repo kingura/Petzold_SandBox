fed0ba2b776900112643ce0cd8c08aa54127dcd6
kopets <kopets@i.ua>
2017-06-16 15:42:03 +0300

    added CaptureLoss

---
+++ CaptureLoss/CaptureLoss.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+class CaptureLoss: Form
+{
+    static public void Main()
+    {
+        Application.Run(new CaptureLoss());
+    }
+    public CaptureLoss()
+    {
+        Text = "Capture Loss";
+
+        // Подключить объект NativeWindow
+        CaptureLossWindow win = new CaptureLossWindow();
+        win.form = this;
+        win.AssignHandle(Handle);
+    }
+    protected override void OnMouseDown(MouseEventArgs e)
+    {
+        Invalidate();
+    }
+    public void OnLostCapture()
+    {
+        Invalidate();
+    }
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        Graphics grfx = e.Graphics;
+
+        if (Capture)
+            grfx.FillRectangle(Brushes.Red, ClientRectangle);
+        else
+            grfx.FillRectangle(Brushes.Gray, ClientRectangle);
+    }
+}
+
+class CaptureLossWindow: NativeWindow
+{
+    public CaptureLoss form;
+
+    protected override void WndProc(ref Message m)
+    {
+        if (m.Msg == 533) // WM_CAPTURECHANGED
+            form.OnLostCapture();
+
+        base.WndProc(ref m);
+    }
+}
