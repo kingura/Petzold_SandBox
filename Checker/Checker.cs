17bfe45d0b37bd5f31277e2214fac77154acab5b
kingura <kopets@i.ua>
2017-09-14 18:06:03 +0300

    added Checker, CheckerWithKeyboard

---
+++ Checker/Checker.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+class Checker:Form
+{
+    protected const int xNum = 5;   // Количество клеток по горизонтали
+    protected const int yNum = 4;   // Количество клеток по вертикали
+    protected int cxBlock, cyBlock;
+    protected bool[,] abChecked = new bool[xNum, yNum];
+
+    public static void Main()
+    {
+        Application.Run(new Checker());
+    }
+
+    public Checker()
+    {
+        Text = "Checker";
+        BackColor = SystemColors.Window;
+        ForeColor = SystemColors.WindowText;
+        ResizeRedraw = true;
+
+        OnResize(EventArgs.Empty);
+    }
+
+    protected override void OnResize(EventArgs e)
+    {
+        base.OnResize(e);   // Иначе ResizeRedraw не будет работать
+
+        cxBlock = ClientSize.Width / xNum;
+        cyBlock = ClientSize.Height / yNum;
+    }
+
+    protected override void OnMouseUp(MouseEventArgs e)
+    {
+        int x = e.X / cxBlock;
+        int y = e.Y / cyBlock;
+
+        if (x < xNum && y < yNum)
+        {
+            abChecked[x, y] ^= true;
+            Invalidate(new Rectangle(x * cxBlock, y * cyBlock, cxBlock, cyBlock));
+        }
+    }
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        base.OnPaint(e);
+
+        Graphics grfx = e.Graphics;
+        Pen pen = new Pen(ForeColor);
+
+        for (int x = 0; x < xNum; x++)
+            for (int y = 0; y < yNum; y++)
+            {
+                grfx.DrawRectangle(pen, x * cxBlock, y * cyBlock, cxBlock, cyBlock);
+                if (abChecked[x, y])
+                {
+                    grfx.DrawLine(pen, x * cxBlock, y * cyBlock, (x + 1) * cxBlock, (y + 1) * cyBlock);
+                    grfx.DrawLine(pen, x * cxBlock, (y + 1) * cyBlock, (x + 1) * cxBlock, y * cyBlock);
+                }
+            }
+    }
+}
---
+++ CheckerWithKeyboard/CheckerWithKeyboard.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+class CheckerWithKeyboard: Checker
+{
+    public new static void Main()
+    {
+        Application.Run(new CheckerWithKeyboard());
+    }
+
+    public CheckerWithKeyboard()
+    {
+        Text += " with Keyboard Interface";
+    }
+
+    protected override void OnGotFocus(EventArgs e)
+    {
+        Cursor.Show();
+    }
+
+    protected override void OnLostFocus(EventArgs e)
+    {
+        Cursor.Hide();
+    }
+
+    protected override void OnKeyDown(KeyEventArgs e)
+    {
+        Point ptCursor = PointToClient(Cursor.Position);
+
+        int x = Math.Max(0, Math.Min(xNum - 1, ptCursor.X / cxBlock));
+        int y = Math.Max(0, Math.Min(yNum - 1, ptCursor.Y / cyBlock));
+
+        switch (e.KeyCode)
+        {
+            case Keys.Up:   y--; break;
+            case Keys.Down: y++; break;
+            case Keys.Left: x--; break;
+            case Keys.Right: x++; break;
+
+            case Keys.Home: x = y = 0; break;
+            case Keys.End: x = xNum - 1; y = yNum - 1; break;
+
+            case Keys.Enter:
+            case Keys.Space:
+                abChecked[x, y] ^= true;
+                Invalidate(new Rectangle(x * cxBlock, y * cyBlock, cxBlock, cyBlock));
+                return;
+
+            default:
+                return;
+        }
+        x = (x + xNum) % xNum;
+        y = (y + yNum) % yNum;
+
+        Cursor.Position = PointToScreen(new Point(x * cxBlock + cxBlock / 2, y * cyBlock + cyBlock / 2));
+    }
+}
