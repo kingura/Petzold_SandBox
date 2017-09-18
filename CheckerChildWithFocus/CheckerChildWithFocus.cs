4d885c89e54ea36f38913a3fe46591acd72feeb6
kingura <kopets@i.ua>
2017-09-18 03:38:45 +0300

    added CheckerChild, CheckerWithChildren, CheckerChildWithFocus, CheckerWithChildrenAndFocus

---
+++ CheckerChildWithFocus/CheckerChildWithFocus.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+class CheckerChildWithFocus : CheckerChild
+{
+    protected override void OnGotFocus(EventArgs e)
+    {
+        Invalidate();
+    }
+
+    protected override void OnLostFocus(EventArgs e)
+    {
+        Invalidate();
+    }
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        base.OnPaint(e);
+        if (Focused)
+        {
+            Graphics grfx = e.Graphics;
+            Pen pen = new Pen(ForeColor, 5);
+
+            grfx.DrawRectangle(pen, ClientRectangle);
+        }
+    }
+}
