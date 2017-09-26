79c318d600a592b6e6465a9f928df749ca209d70
kingura <kopets@i.ua>
2017-09-26 16:23:17 +0300

    added BoldAndItalic, FontNames, FontSizes, BoldAndItalicBigger

---
+++ BoldAndItalicBigger/BoldAndItalicBigger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+class BoldAndItalicBigger : BoldAndItalic
+{
+    public new static void Main()
+    {
+        Application.Run(new BoldAndItalicBigger());
+    }
+
+    public BoldAndItalicBigger()
+    {
+        Text += " Bigger";
+        Font = new Font("Times New Roman", 24);
+    }
+}
---
+++ FontSizes/FontSizes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+class FontSizes: PrintableForm
+{
+    public new static void Main()
+    {
+        Application.Run(new FontSizes());
+    }
+
+    public FontSizes()
+    {
+        Text = "Font Sizes";
+    }
+
+    protected override void DoPage(Graphics grfx, Color clr, int cx, int cy)
+    {
+        string strFont = "Comic Sans MS";// "Times New Roman"; //{ "Comic Sans MS" };
+        Brush brush = new SolidBrush(clr);
+        float y = 0;
+
+        for (float fSize = 6; fSize <= 12; fSize += 0.25f)
+        {
+            Font font = new Font(strFont, fSize);
+            grfx.DrawString(strFont + " in " + fSize + " points", font, brush, 0, y);
+            y += font.GetHeight(grfx);
+        }
+    }
+}
