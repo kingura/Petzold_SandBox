b5336faddd4ca2f38881da68427eba82afe8c808
kingura <kopets@i.ua>
2017-10-02 19:24:31 +0300

    added InstalledFontsList, AntiAliasedText

---
+++ InstalledFontsList/InstalledFontsList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Windows.Forms;
+
+class InstalledFontsList : FamiliesList
+{
+    public new static void Main()
+    {
+        Application.Run(new InstalledFontsList());
+    }
+
+    public InstalledFontsList()
+    {
+        Text = "InstalledFontsCollection List";
+    }
+
+    protected override FontFamily[] GetFontFamilyArray(Graphics grfx)
+    {
+        FontCollection fc = new InstalledFontCollection();
+        return fc.Families;
+        
+    }
+}
