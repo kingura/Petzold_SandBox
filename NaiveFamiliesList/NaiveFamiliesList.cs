920ea4de0dbc290c430694086d8f8070c6481d0f
kingura <kopets@i.ua>
2017-10-02 13:43:23 +0300

    added TextOnBaseline, NaiveFamiliesList, BetterFamiliesList, FamiliesList, GetFamiliesList

---
+++ NaiveFamiliesList/NaiveFamiliesList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+class NaiveFamiliesList: PrintableForm
+{
+    public new static void Main()
+    {
+        Application.Run(new NaiveFamiliesList());
+    }
+
+    public NaiveFamiliesList()
+    {
+        Text = "Naive Font Families List";
+    }
+
+    protected override void DoPage(Graphics grfx, Color clr, int cx, int cy)
+    {
+        Brush brush = new SolidBrush(clr);
+        float y = 0;
+        FontFamily[] aff = FontFamily.Families;
+
+        foreach (FontFamily ff in aff)
+        {
+            try
+            {
+                Font font = new Font(ff, 12);
+                grfx.DrawString(ff.Name, font, brush, 0, y);
+                y += font.GetHeight(grfx);
+            }
+            catch
+            {
+                grfx.DrawString("*" + ff.Name, Font, brush, 0, y);
+                y += Font.GetHeight(grfx);
+            }
+        }
+    }
+}
---
+++ TextOnBaseline/TextOnBaseline.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+class TextOnBaseline: PrintableForm
+{
+    public new static void Main()
+    {
+        Application.Run(new TextOnBaseline());
+    }
+
+    public TextOnBaseline()
+    {
+        Text = "Text On Baseline";
+    }
+
+    protected override void DoPage(Graphics grfx, Color clr, int cx, int cy)
+    {
+        float yBaseline = cy / 2;
+        Pen pen = new Pen(clr);
+
+        // Рисуем базовую линию в центре клиентской области
+
+        grfx.DrawLine(pen, 0, yBaseline, cx, yBaseline);
+
+        // Создаем 144-пунктный шрифт
+
+        Font font = new Font("Times New Roman", 100);
+
+        // Получаем и вычисляем метрики
+
+        float cyLineSpace = font.GetHeight(grfx);
+        int iCellSpace = font.FontFamily.GetLineSpacing(font.Style);
+        int iCellAscent = font.FontFamily.GetCellAscent(font.Style);
+        float cyAscent = cyLineSpace * iCellAscent / iCellSpace;
+
+        // Отображаем текст на базовой линии
+
+        grfx.DrawString("Baseline", font, new SolidBrush(clr), 0, yBaseline - cyAscent);
+
+    }
+}
