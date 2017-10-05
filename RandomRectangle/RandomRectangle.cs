ac72e0c011ad19f366d539b6aa462cd15ae513bd
kingura <kopets@i.ua>
2017-10-06 02:58:51 +0300

    added CloseInFive, RandomRectangle

---
+++ RandomRectangle/RandomRectangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+class RandomRectangle : Form
+{
+    public static void Main()
+    {
+        Application.Run(new RandomRectangle());
+    }
+
+    public RandomRectangle()
+    {
+        Text = "Random Rectangle";
+
+        Timer timer = new Timer();
+        timer.Interval = 1;
+        timer.Tick += new EventHandler(TimerOnTick);
+        timer.Start();
+    }
+
+    void TimerOnTick(object obj, EventArgs e)
+    {
+        Random rand = new Random();
+
+        int x1 = rand.Next(ClientSize.Width);
+        int x2 = rand.Next(ClientSize.Width);
+        int y1 = rand.Next(ClientSize.Height);
+        int y2 = rand.Next(ClientSize.Height);
+
+        Color color = Color.FromArgb(rand.Next(256), rand.Next(256), rand.Next(256));
+
+        Graphics grfx = CreateGraphics();
+        grfx.FillRectangle(new SolidBrush(color),
+                           Math.Min(x1, x2), Math.Min(y1, y2),
+                           Math.Abs(x1 - x2),Math.Abs(y1 - y2));
+        grfx.Dispose();
+    }
+}
