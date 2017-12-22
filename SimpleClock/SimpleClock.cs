255bf66a56d74d7cba1438d3cd1aa3a18a0372b1
kingura <kopets@i.ua>
2017-12-22 06:58:45 +0200

    added SimpleClock

---
+++ SimpleClock/SimpleClock.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+class SimpleClock : Form
+{
+    public static void Main()
+    {
+        Application.Run(new SimpleClock());
+    }
+
+    public SimpleClock()
+    {
+        Text = "Simple Clock";
+        ForeColor = SystemColors.WindowText;
+        BackColor = SystemColors.Window;
+        ResizeRedraw = true;
+
+        Timer timer = new Timer();
+        timer.Tick += new EventHandler(TimerOnTick);
+        timer.Interval = 1000;
+        timer.Start();
+    }
+
+    private void TimerOnTick(object obj, EventArgs ea)
+    {
+        Invalidate();
+    }
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        base.OnPaint(e);
+
+        StringFormat strfmt = new StringFormat();
+        strfmt.Alignment = strfmt.LineAlignment = StringAlignment.Center;
+
+        e.Graphics.DrawString(DateTime.Now.ToString("F"), Font, new SolidBrush(ForeColor),
+            ClientRectangle, strfmt);
+    }
+}
