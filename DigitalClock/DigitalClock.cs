c15e5074aa11e7f635c6a298c6b68423b8afadf1
kingura <kopets@i.ua>
2017-12-25 14:58:15 +0200

    added DigitalClock, DigitalClockWithDate

---
+++ DigitalClock/DigitalClock.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+class DigitalClock : Form
+{
+    public static void Main()
+    {
+        Application.Run(new DigitalClock());
+    }
+
+    public DigitalClock()
+    {
+        Text = "Digital Clock";
+        ForeColor = SystemColors.WindowText;
+        BackColor = SystemColors.Window;
+        ResizeRedraw = true;
+
+        Timer timer = new Timer();
+        timer.Tick += new EventHandler(TimerOnTick);
+        timer.Interval = 1000;
+        timer.Start();
+    }
+
+    private void TimerOnTick(object obj, EventArgs ea)
+    {
+        Invalidate();
+    }
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        Graphics grfx = e.Graphics;
+        string strTime = DateTime.Now.ToString("T");
+        SizeF sizef = grfx.MeasureString(strTime, Font);
+        float fScale = Math.Min(ClientSize.Width / sizef.Width, ClientSize.Height / sizef.Height);
+        Font font = new Font(Font.FontFamily, fScale * Font.SizeInPoints);
+
+        sizef = grfx.MeasureString(strTime, font);
+
+        grfx.DrawString(strTime, font, new SolidBrush(ForeColor),
+            (ClientSize.Width - sizef.Width) / 2, (ClientSize.Height - sizef.Height) / 2);
+    }
+}
---
+++ DigitalClockWithDate/DigitalClockWithDate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+class DigitalClockWithDate: DigitalClock
+{
+    public new static void Main()
+    {
+        Application.Run(new DigitalClockWithDate());
+    }
+
+    public DigitalClockWithDate()
+    {
+        Text += " with Date";
+    }
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        Graphics grfx = e.Graphics;
+        DateTime dt = DateTime.Now;
+        string strTime = dt.ToString("d") + "\n" + dt.ToString("T");
+        SizeF sizef = grfx.MeasureString(strTime, Font);
+        float fScale = Math.Min(ClientSize.Width / sizef.Width, ClientSize.Height / sizef.Height);
+        Font font = new Font(Font.FontFamily, fScale * Font.SizeInPoints);
+
+        StringFormat strfmt = new StringFormat();
+        strfmt.Alignment = strfmt.LineAlignment = StringAlignment.Center;
+
+        grfx.DrawString(strTime, font, new SolidBrush(ForeColor), ClientRectangle, strfmt);
+    }
+}
