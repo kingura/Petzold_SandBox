82271ca38c02d7fd5b89bf14eef251e3b67ab6b5
kingura <kopets@i.ua>
2017-12-29 14:46:17 +0200

    added AnalogClock

---
+++ AnalogClock/AnalogClock.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+class AnalogClock : Form
+{
+    ClockControl clkctl;
+
+    public static void Main()
+    {
+        Application.Run(new AnalogClock());
+    }
+    public AnalogClock()
+    {
+        Text = "Analog Clock";
+        BackColor = SystemColors.Window;
+        ForeColor = SystemColors.WindowText;
+
+        clkctl = new ClockControl();
+        clkctl.Parent = this;
+        clkctl.Time = DateTime.Now;
+        clkctl.Dock = DockStyle.Fill;
+        clkctl.BackColor = Color.Black;
+        clkctl.ForeColor = Color.White;
+
+        Timer timer = new Timer();
+        timer.Interval = 100;
+        timer.Tick += new EventHandler(TimerOnTick);
+        timer.Start();
+    }
+
+    void TimerOnTick(object obj, EventArgs ea)
+    {
+        //clkctl.Time = DateTime.Now;
+
+        DateTime dt = DateTime.Now;
+        dt = new DateTime(dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, dt.Second);
+        clkctl.Time = dt;
+
+        //clkctl.Time += new TimeSpan(10000000);
+
+        //clkctl.Time -= new TimeSpan(1000000);
+    }
+}
