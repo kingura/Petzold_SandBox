478083834633ce92b7b0a5bdcd9789fd5bd8ec7d
kingura <kopets@i.ua>
2018-01-16 18:11:19 +0200

    added ImageFromFile, BetterImageFromFile, ImageFromWeb

---
+++ ImageFromFile/ImageFromFile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+class ImageFromFile: PrintableForm
+{
+    public new static void Main()
+    {
+        Application.Run(new ImageFromFile());
+    }
+
+    public ImageFromFile()
+    {
+        Text = "Image From File";
+    }
+
+    protected override void DoPage(Graphics grfx, Color clr, int cx, int cy)
+    {
+        Image image = Image.FromFile("..\\..\\..\\Apollo11FullColor_220x240.jpg");
+
+        grfx.DrawImage(image, 0, 0);
+    }
+
+}
---
+++ ImageFromWeb/ImageFromWeb.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+class ImageFromWeb : PrintableForm
+{
+    Image image;
+
+    public new static void Main()
+    {
+        Application.Run(new ImageFromWeb());
+    }
+
+    public ImageFromWeb()
+    {
+        Text = "Image From Web";
+        
+        string strUrl = "https://spaceflight.nasa.gov/gallery/images/apollo/apollo11/lores/as11_40_5903.jpg";
+
+        WebRequest webreq = WebRequest.Create(strUrl);
+        WebResponse webres = webreq.GetResponse();
+        Stream stream = webres.GetResponseStream();
+
+        image = Image.FromStream(stream);
+        stream.Close();
+    }
+
+    protected override void DoPage(Graphics grfx, Color clr, int cx, int cy)
+    {
+        grfx.DrawImage(image, 0, 0);
+    }
+}
