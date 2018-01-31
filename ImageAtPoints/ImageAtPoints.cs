6ab0902f60699ebbaf0f7157769e582d416eb8af
kingura <kopets@i.ua>
2018-01-31 10:35:26 +0200

    added ImageAtPoints, PartialImage, PartialImageStretch, PartialImageRotate

---
+++ ImageAtPoints/ImageAtPoints.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+class ImageAtPoints: PrintableForm
+{
+  Image image;
+
+  public static new void Main()
+  {
+    Application.Run(new ImageAtPoints());
+  }
+
+  public ImageAtPoints()
+  {
+    Text = "Image At Points";
+
+    image = Image.FromFile("..\\..\\..\\Apollo11FullColor_220x240.jpg");
+  }
+
+  protected override void DoPage(Graphics grfx, Color clr, int cx, int cy)
+  {
+    grfx.DrawImage(image, new Point[] { new Point(cx / 2, 0), new Point(cx, cy / 2), new Point(0, cy / 2) });
+  }
+}
---
+++ PartialImage/PartialImage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+class PartialImage : PrintableForm
+{
+  Image image;
+
+  public new static void Main()
+  {
+    Application.Run(new PartialImage());
+  }
+
+  public PartialImage()
+  {
+    Text = "Partial Image";
+
+    image = Image.FromFile("..\\..\\..\\Apollo11FullColor_220x240.jpg");
+  }
+
+  protected override void DoPage(Graphics grfx, Color clr, int cx, int cy)
+  {
+    Rectangle rect = new Rectangle(95, 5, 50, 55);
+
+    grfx.DrawImage(image, 0, 0, rect, GraphicsUnit.Pixel);
+  }
+}
