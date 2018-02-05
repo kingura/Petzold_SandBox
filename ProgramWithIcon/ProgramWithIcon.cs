3b132da2c54342919ee4c497869cf7ebee0a0214
kingura <kopets@i.ua>
2018-02-05 11:04:10 +0200

    added ProgramWithIcon

---
+++ ProgramWithIcon/ProgramWithIcon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+internal class ProgramWithIcon : Form
+{
+  public static void Main()
+  {
+    Application.Run(new ProgramWithIcon());
+  }
+
+  public ProgramWithIcon()
+  {
+    Text = "Program With Icon";
+
+    Icon = new Icon(typeof(ProgramWithIcon), "ProgramWithIcon.ProgramWithIcon.ico");
+  }
+}
