69d5f43dec4c2a560e31b9590e0b1820c556d88c
kingura <kopets@i.ua>
2018-02-07 06:23:52 +0200

    added PictureBoxPlus, PictureBoxPlusDemo

---
+++ PictureBoxPlus/PictureBoxPlus.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Petzold.ProgrammingWindowsWithCSharp
+{
+  public class PictureBoxPlus : PictureBox
+  {
+    bool bNoDistort = false;
+
+    public bool NoDistort
+    {
+      get { return bNoDistort; }
+      set
+      {
+        bNoDistort = value;
+        Invalidate();
+      }
+    }
+
+    protected override void OnPaint(PaintEventArgs pe)
+    {
+      if ((Image != null) && bNoDistort && (SizeMode == PictureBoxSizeMode.StretchImage))
+      {
+        ScaleImageIsotropically(pe.Graphics, Image, ClientRectangle);
+      }
+      else
+        base.OnPaint(pe);
+    }
+
+    void ScaleImageIsotropically(Graphics grfx, Image image, Rectangle rect)
+    {
+      SizeF sizef = new SizeF(image.Width / image.HorizontalResolution,
+                       image.Height / image.VerticalResolution);
+
+      float fScale = Math.Min(rect.Width / sizef.Width,
+                               rect.Height / sizef.Height);
+
+      sizef.Width *= fScale;
+      sizef.Height *= fScale;
+
+      grfx.DrawImage(image, rect.X + (rect.Width - sizef.Width) / 2,
+                            rect.Y + (rect.Height - sizef.Height) / 2,
+                            sizef.Width, sizef.Height);
+    }
+  }
+}
---
+++ PictureBoxPlusDemo/PictureBoxPlusDemo.cs
@@ -0,0 +1,24 @@
+﻿using Petzold.ProgrammingWindowsWithCSharp;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+class PictureBoxPlusDemo : Form
+{
+  public static void Main()
+  {
+    Application.Run(new PictureBoxPlusDemo());
+  }
+
+  public PictureBoxPlusDemo()
+  {
+    Text = "PictureBoxPlus Demo";
+
+    PictureBoxPlus picbox = new PictureBoxPlus();
+    picbox.Parent = this;
+    picbox.Dock = DockStyle.Fill;
+    picbox.Image = Image.FromFile("..\\..\\..\\Apollo11FullColor_220x240.jpg");
+    picbox.SizeMode = PictureBoxSizeMode.StretchImage;
+    picbox.NoDistort = true;
+  }
+}
