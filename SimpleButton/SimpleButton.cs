1c344c0c798337a94e6e1c4bf0ae08689ef503f8
kingura <kopets@i.ua>
2018-02-07 20:00:31 +0200

    added Simple Button

---
+++ SimpleButton/SimpleButton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+class SimpleButton : Form
+{
+    public static void Main()
+    {
+        Application.Run(new SimpleButton());
+    }
+
+    public SimpleButton()
+    {
+        Text = "Simple Button";
+
+        Button button = new Button();
+        button.Parent = this;
+        button.Text = "Click me!";
+        button.Location = new Point(100, 100);
+        button.Click += new EventHandler(ButtonOnClick);
+    }
+
+    void ButtonOnClick(object obj, EventArgs ea)
+    {
+        Graphics grfx = CreateGraphics();
+        Point ptText = Point.Empty;
+        string str = "Button Clicked!";
+
+        grfx.DrawString(str, Font, new SolidBrush(ForeColor), ptText);
+        System.Threading.Thread.Sleep(250);
+        grfx.FillRectangle(new SolidBrush(BackColor),
+            new RectangleF(ptText, grfx.MeasureString(str, Font)));
+        grfx.Dispose();
+    }
+}
