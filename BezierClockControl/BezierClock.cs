dcb4b6fc2898b6cf296ece7cff6bd84ec30e0728
kingura <kopets@i.ua>
2018-02-21 18:36:40 +0200

    added BezierClock, Infinity

---
+++ BezierClockControl/BezierClock.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Petzold.ProgrammingWindowsWithCSharp;
+
+class BezierClock : Form
+{
+  BezierClockControl clkctl;
+
+  public static void Main()
+  {
+    Application.Run(new BezierClock());
+  }
+
+  public BezierClock()
+  {
+    Text = "Bezier Clock";
+
+    clkctl = new BezierClockControl();
+    clkctl.Parent = this;
+    clkctl.Time = DateTime.Now;
+    clkctl.Dock = DockStyle.Fill;
+    clkctl.BackColor = Color.Black;
+    clkctl.ForeColor = Color.White;
+
+    Timer timer = new Timer();
+    timer.Interval = 100;
+    timer.Tick += OnTimerTick;
+    timer.Start();
+  }
+
+  void OnTimerTick(object obj, EventArgs e)
+  {
+    clkctl.Time = DateTime.Now;
+  }
+}
---
+++ Infinity/Infinity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+class Infinity : PrintableForm
+{
+  public new static void Main()
+  {
+    Application.Run(new Infinity());
+  }
+
+  public Infinity()
+  {
+    Text = "Infinity Sign Using Bezier Splines";
+  }
+
+  protected override void DoPage(Graphics grfx, Color clr, int cx, int cy)
+  {
+    cx--;
+    cy--;
+
+    Point[] apt =
+    {
+      new Point(0,          cy / 2),     // Начальная
+      new Point(0,          0),          // Управляющая
+      new Point(    cx / 3, 0),          // Управляющая
+      new Point(    cx / 2, cy / 2),     // Конечная/начальная
+      new Point(2 * cx / 3, cy),         // Управляющая
+      new Point(    cx,     cy),         // Управляющая
+      new Point(    cx,     cy / 2),     // Конечная/начальная
+      new Point(    cx,     0),          // Управляющая
+      new Point(2 * cx / 3, 0),          // Управляющая
+      new Point(    cx / 2, cy / 2),     // Конечная/начальная
+      new Point(    cx / 3, cy),         // Управляющая
+      new Point(0,          cy),         // Управляющая
+      new Point(0,          cy / 2)      // Конечная
+    };
+    grfx.DrawBeziers(new Pen(clr), apt);
+    //grfx.DrawPolygon(new Pen(Color.Gray), apt);
+  }
+}
