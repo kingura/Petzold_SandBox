f5b45259d9a5e4449ce7203d3e84db62fe48dfff
kingura <kopets@i.ua>
2018-03-01 11:06:26 +0200

    added StandardMenu, OwnerDrawMenu, HelpMenu

---
+++ OwnerDrawMenu/OwnerDrawMenu.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Text;    // Для перечисления HotkeyPrefix
+using System.Windows.Forms;
+
+class OwnerDrawMenu : Form
+{
+  const int iFontPointSize = 18; // Для пунктов меню
+  MenuItem miFacename;
+
+  public static void Main()
+  {
+    Application.Run(new OwnerDrawMenu());
+  }
+  public OwnerDrawMenu()
+  {
+    Text = "Owner-Draw Menu";
+
+    // Пункты верхнего уровня
+
+    Menu = new MainMenu();
+    Menu.MenuItems.Add("&Facename");
+
+    // Массив пунктов подменю
+
+    string[] astrText = { "&Times New Roman", "&Arial", "&Courier New" };
+    MenuItem[] ami = new MenuItem[astrText.Length];
+
+    EventHandler ehOnClick = new EventHandler(MenuFacenameOnClick);
+    MeasureItemEventHandler ehOnMeasureItem =
+              new MeasureItemEventHandler(MenuFacenameOnMeasureItem);
+    DrawItemEventHandler ehOnDrawItem =
+              new DrawItemEventHandler(MenuFacenameOnDrawItem);
+
+    for (int i = 0; i < ami.Length; i++)
+    {
+      ami[i] = new MenuItem(astrText[i]);
+      ami[i].OwnerDraw = true;
+      ami[i].RadioCheck = true;
+      ami[i].Click += ehOnClick;
+      ami[i].MeasureItem += ehOnMeasureItem;
+      ami[i].DrawItem += ehOnDrawItem;
+    }
+    miFacename = ami[0];
+    miFacename.Checked = true;
+
+    Menu.MenuItems[0].MenuItems.AddRange(ami);
+  }
+  void MenuFacenameOnClick(object obj, EventArgs ea)
+  {
+    miFacename.Checked = false;
+    miFacename = (MenuItem)obj;
+    miFacename.Checked = true;
+
+    Invalidate();
+  }
+  void MenuFacenameOnMeasureItem(object obj, MeasureItemEventArgs miea)
+  {
+    MenuItem mi = (MenuItem)obj;
+    Font font = new Font(mi.Text.Substring(1), iFontPointSize);
+
+    StringFormat strfmt = new StringFormat();
+    strfmt.HotkeyPrefix = HotkeyPrefix.Show;
+
+    SizeF sizef = miea.Graphics.MeasureString(mi.Text, font,
+                                              1000, strfmt);
+
+    miea.ItemWidth = (int)Math.Ceiling(sizef.Width);
+    miea.ItemHeight = (int)Math.Ceiling(sizef.Height);
+
+    miea.ItemWidth += SystemInformation.MenuCheckSize.Width *
+                        miea.ItemHeight /
+                             SystemInformation.MenuCheckSize.Height;
+
+    miea.ItemWidth -= SystemInformation.MenuCheckSize.Width;
+  }
+  void MenuFacenameOnDrawItem(object obj, DrawItemEventArgs diea)
+  {
+    MenuItem mi = (MenuItem)obj;
+    Graphics grfx = diea.Graphics;
+    Brush brush;
+
+    // Создание Font и StringFormat.
+
+    Font font = new Font(mi.Text.Substring(1), iFontPointSize);
+    StringFormat strfmt = new StringFormat();
+    strfmt.HotkeyPrefix = HotkeyPrefix.Show;
+
+    // Вычисление прямоугольников флажка и текста
+
+    Rectangle rectCheck = diea.Bounds;
+
+    rectCheck.Width = SystemInformation.MenuCheckSize.Width *
+                        rectCheck.Height /
+                             SystemInformation.MenuCheckSize.Height;
+
+    Rectangle rectText = diea.Bounds;
+
+    rectText.X += rectCheck.Width;
+
+    // Выполняем все рисование
+
+    diea.DrawBackground();
+
+    if ((diea.State & DrawItemState.Checked) != 0)
+      ControlPaint.DrawMenuGlyph(grfx, rectCheck, MenuGlyph.Bullet);
+
+    if ((diea.State & DrawItemState.Selected) != 0)
+      brush = SystemBrushes.HighlightText;
+    else
+      brush = SystemBrushes.FromSystemColor(SystemColors.MenuText);
+
+    grfx.DrawString(mi.Text, font, brush, rectText, strfmt);
+  }
+  protected override void OnPaint(PaintEventArgs pea)
+  {
+    Graphics grfx = pea.Graphics;
+    Font font = new Font(miFacename.Text.Substring(1), 12);
+    StringFormat strfmt = new StringFormat();
+    strfmt.Alignment = strfmt.LineAlignment = StringAlignment.Center;
+
+    grfx.DrawString(Text, font, new SolidBrush(ForeColor), 0, 0);
+  }
+}
---
+++ StandardMenu/StandardMenu.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+class StandardMenu : Form
+{
+  MenuItem miFileOpen, miFileSave;
+  MenuItem miEditCut, miEditCopy, miEditPaste;
+
+  // Экспериментальные переменные для кода Popup
+
+  bool bDocumentPresent = true;
+  bool bNonNullSelection = true;
+  bool bStuffInClipboard = false;
+
+  public static void Main()
+  {
+    Application.Run(new StandardMenu());
+  }
+  public StandardMenu()
+  {
+    Text = "Standard Menu";
+    Menu = new MainMenu();
+
+    // File
+
+    MenuItem mi = new MenuItem("&File");
+    mi.Popup += new EventHandler(MenuFileOnPopup);
+    Menu.MenuItems.Add(mi);
+    int index = Menu.MenuItems.Count - 1;
+
+    // File - Open
+
+    miFileOpen = new MenuItem("&Open...");
+    miFileOpen.Click += new EventHandler(MenuFileOpenOnClick);
+    miFileOpen.Shortcut = Shortcut.CtrlO;
+    Menu.MenuItems[index].MenuItems.Add(miFileOpen);
+
+    // File - Save
+
+    miFileSave = new MenuItem("&Save");
+    miFileSave.Click += new EventHandler(MenuFileSaveOnClick);
+    miFileSave.Shortcut = Shortcut.CtrlS;
+    Menu.MenuItems[index].MenuItems.Add(miFileSave);
+
+    // Горизонтальная линия
+
+    mi = new MenuItem("-");
+    Menu.MenuItems[index].MenuItems.Add(mi);
+
+    // File - Exit
+
+    mi = new MenuItem("E&xit");
+    mi.Click += new EventHandler(MenuFileExitOnClick);
+    Menu.MenuItems[index].MenuItems.Add(mi);
+
+    // Edit
+
+    mi = new MenuItem("&Edit");
+    mi.Popup += new EventHandler(MenuEditOnPopup);
+    Menu.MenuItems.Add(mi);
+    index = Menu.MenuItems.Count - 1;
+
+    // Edit - Cut
+
+    miEditCut = new MenuItem("Cu&t");
+    miEditCut.Click += new EventHandler(MenuEditCutOnClick);
+    miEditCut.Shortcut = Shortcut.CtrlX;
+    Menu.MenuItems[index].MenuItems.Add(miEditCut);
+
+    // Edit - Copy
+
+    miEditCopy = new MenuItem("&Copy");
+    miEditCopy.Click += new EventHandler(MenuEditCopyOnClick);
+    miEditCopy.Shortcut = Shortcut.CtrlC;
+    Menu.MenuItems[index].MenuItems.Add(miEditCopy);
+
+    // Edit - Paste
+
+    miEditPaste = new MenuItem("&Paste");
+    miEditPaste.Click += new EventHandler(MenuEditCopyOnClick);
+    miEditPaste.Shortcut = Shortcut.CtrlV;
+    Menu.MenuItems[index].MenuItems.Add(miEditPaste);
+
+    // Help
+
+    mi = new MenuItem("&Help");
+    Menu.MenuItems.Add(mi);
+    index = Menu.MenuItems.Count - 1;
+
+    // Help - About
+
+    mi = new MenuItem("&About StandardMenu...");
+    mi.Click += new EventHandler(MenuHelpAboutOnClick);
+    Menu.MenuItems[index].MenuItems.Add(mi);
+  }
+  void MenuFileOnPopup(object obj, EventArgs ea)
+  {
+    miFileSave.Enabled = bDocumentPresent;
+  }
+  void MenuEditOnPopup(object obj, EventArgs ea)
+  {
+    miEditCut.Enabled = bNonNullSelection;
+    miEditCopy.Enabled = bNonNullSelection;
+    miEditPaste.Enabled = bStuffInClipboard;
+  }
+  void MenuFileOpenOnClick(object obj, EventArgs ea)
+  {
+    MessageBox.Show("This should be a File Open dialog box!", Text);
+  }
+  void MenuFileSaveOnClick(object obj, EventArgs ea)
+  {
+    MessageBox.Show("This should be a File Save dialog box!", Text);
+  }
+  void MenuFileExitOnClick(object obj, EventArgs ea)
+  {
+    Close();
+  }
+  void MenuEditCutOnClick(object obj, EventArgs ea)
+  {
+    // Копирование выбранного фрагмента в буфер обмена
+    // и удаление его из документа
+  }
+  void MenuEditCopyOnClick(object obj, EventArgs ea)
+  {
+    // Копирование выбранного фрагмента в буфер обмена
+  }
+  void MenuEditPasteOnClick(object obj, EventArgs ea)
+  {
+    // Копирование данных из буфера обмена в документ
+  }
+  void MenuHelpAboutOnClick(object obj, EventArgs ea)
+  {
+    MessageBox.Show("StandardMenu (C) 2018 by Joe Black", Text);
+  }
+}
