2c49d0d9d7f39b0f04ca009ed77f80be1efb4ac2
kingura <kopets@i.ua>
2018-03-05 13:35:08 +0200

    added LineArcCombo, WidePolyline, LineArcPath

---
+++ LineArcPath/LineArcPath.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+class LineArcPath : PrintableForm
+{
+  public new static void Main()
+  {
+    Application.Run(new LineArcPath());
+  }
+
+  public LineArcPath()
+  {
+    Text = "LineArcPath";
+    Size = new Size((int)(Size.Width * 1.3), (int)(Size.Height / 1.5));
+  }
+
+  protected override void DoPage(Graphics grfx, Color clr, int cx, int cy)
+  {
+    GraphicsPath path = new GraphicsPath();
+    Pen pen = new Pen(clr, 25);
+
+    path.AddLine(25, 100, 125, 100);
+    path.AddArc(125, 50, 100, 100, -180, 180);
+    path.AddLine(225, 100, 325, 100);
+
+    grfx.DrawPath(pen, path);
+  }
+}
---
+++ WidePolyline/WidePolyline.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+class WidePolyline : PrintableForm
+{
+  public new static void Main()
+  {
+    Application.Run(new WidePolyline());
+  }
+  public WidePolyline()
+  {
+    Text = "Wide Polyline";
+    Size = new Size((int)(Size.Width * 1.3), (int)(Size.Height / 1.5));
+  }
+  protected override void DoPage(Graphics grfx, Color clr, int cx, int cy)
+  {
+    Pen pen = new Pen(clr, 25);
+
+    grfx.DrawLines(pen, new Point[] {
+                         new Point( 25, 100), new Point(125, 100),
+                         new Point(125,  50), new Point(225,  50),
+                         new Point(225, 100), new Point(325, 100) });
+  }
+}
