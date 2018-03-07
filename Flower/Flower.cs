f8ac7a15a191d073f509bbae798d2260450f8442
kingura <kopets@i.ua>
2018-03-07 20:22:34 +0200

    added Flower, ScribbleWithPath

---
+++ Flower/Flower.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+class Flower : PrintableForm
+{
+  public new static void Main()
+  {
+    Application.Run(new Flower());
+  }
+  public Flower()
+  {
+    Text = "Flower";
+  }
+  protected override void DoPage(Graphics grfx, Color clr, int cx, int cy)
+  {
+    // Рисуем зеленый стебель из нижнего левого угла к центру
+
+    grfx.DrawBezier(new Pen(Color.Green, 10),
+              new Point(0, cy), new Point(0, 3 * cy / 4),
+              new Point(cx / 4, cy / 4), new Point(cx / 2, cy / 2));
+
+    // Устанавливаем преобразование для оставшегося цветка
+
+    float fScale = Math.Min(cx, cy) / 2000f;
+    grfx.TranslateTransform(cx / 2, cy / 2);
+    grfx.ScaleTransform(fScale, fScale);
+
+    // Рисуем красные лепестки
+
+    GraphicsPath path = new GraphicsPath();
+
+    path.AddBezier(new Point(0, 0), new Point(125, 125),
+                   new Point(475, 125), new Point(600, 0));
+    path.AddBezier(new Point(600, 0), new Point(475, -125),
+                   new Point(125, -125), new Point(0, 0));
+
+    for (int i = 0; i < 8; i++)
+    {
+      grfx.FillPath(Brushes.Red, path);
+      grfx.DrawPath(Pens.Black, path);
+      grfx.RotateTransform(360 / 8);
+    }
+
+    // Рисуем желтый круг в центре
+
+    Rectangle rect = new Rectangle(-150, -150, 300, 300);
+    grfx.FillEllipse(Brushes.Yellow, rect);
+    grfx.DrawEllipse(Pens.Black, rect);
+  }
+}
---
+++ ScribbleWithPath/ScribbleWithPath.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+class ScribbleWithPath: Form
+{
+  GraphicsPath path;
+  bool bTracking;
+  Point ptLast;
+
+  public static void Main()
+  {
+    Application.Run(new ScribbleWithPath());
+  }
+
+  public ScribbleWithPath()
+  {
+    Text = "Scribble with Path";
+    BackColor = SystemColors.Window;
+    ForeColor = SystemColors.WindowText;
+
+    // Создаем контур
+
+    path = new GraphicsPath();
+  }
+
+  protected override void OnMouseDown(MouseEventArgs e)
+  {
+    if (e.Button != MouseButtons.Left)
+      return;
+
+    ptLast = e.Location;
+    bTracking = true;
+
+    // Начало фигуры
+
+    path.StartFigure();
+  }
+
+  protected override void OnMouseMove(MouseEventArgs e)
+  {
+    if (!bTracking)
+      return;
+
+    Point ptNew = e.Location;
+
+    Graphics grfx = CreateGraphics();
+    grfx.DrawLine(new Pen(ForeColor), ptLast, ptNew);
+    grfx.Dispose();
+
+    // Добавляем линию
+
+    path.AddLine(ptLast, ptNew);
+
+    ptLast = ptNew;
+  }
+
+  protected override void OnMouseUp(MouseEventArgs e)
+  {
+    bTracking = false;
+  }
+
+  protected override void OnPaint(PaintEventArgs e)
+  {
+    // Рисуем контур
+
+    e.Graphics.DrawPath(new Pen(ForeColor), path);
+  }
+}
