6a83abb4577ed0496a1c902c59f5a2806437fa53
kingura <kopets@i.ua>
2018-04-23 17:51:51 +0300

    added HatchBrushArray

---
+++ HatchBrushArray/HatchBrushArray.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+class HatchBrushArray : PrintableForm
+{
+  const int iSize = 32, iMargin = 8;
+  const int iMin = 0, iMax = 52; // Минимальное и максимальное значения HatchStyle
+
+  public new static void Main()
+  {
+    Application.Run(new HatchBrushArray());
+  }
+
+  public HatchBrushArray()
+  {
+    Text = "Hatch Brush Array";
+    ClientSize = new Size(8 * iSize + 9 * iMargin, 7 * iSize + 8 * iMargin);
+  }
+
+  protected override void DoPage(Graphics grfx, Color clr, int cx, int cy)
+  {
+    for (HatchStyle hs = (HatchStyle)iMin; hs <= (HatchStyle)iMax; hs++)
+    {
+      HatchBrush hbrush = new HatchBrush(hs, Color.White, Color.Black);
+      int y = (int)hs / 8;
+      int x = (int)hs % 8;
+
+      grfx.FillRectangle(hbrush, iMargin + x * (iMargin + iSize),
+                                 iMargin + y * (iMargin + iSize),
+                                 iSize, iSize);
+    }
+  }
+}
