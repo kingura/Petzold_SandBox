6b9fda19e93e5bc2d25c791c81c770400607411d
kingura <kopets@i.ua>
2018-04-30 16:01:13 +0300

    added TwoPointLinearGradientBrush

---
+++ TwoPointLinearGradientBrush/TwoPointLinearGradientBrush.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+class TwoPointLinearGradientBrush : PrintableForm
+{
+  public new static void Main()
+  {
+    Application.Run(new TwoPointLinearGradientBrush());
+  }
+
+  public TwoPointLinearGradientBrush()
+  {
+    Text = "Two-Point Linear Gradient Brush";
+  }
+
+  protected override void DoPage(Graphics grfx, Color clr, int cx, int cy)
+  {
+    LinearGradientBrush lgbrush = new LinearGradientBrush(
+      new Point(cx / 4, cy / 4),
+      new Point(cx * 3 / 4, cy * 3 / 4),
+      Color.White, Color.Black);
+
+    lgbrush.WrapMode = WrapMode.TileFlipX;
+
+    grfx.FillRectangle(lgbrush, 0, 0, cx, cy);
+  }
+}
