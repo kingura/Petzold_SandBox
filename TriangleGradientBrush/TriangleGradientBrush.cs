110d0e31ca06d5c2c03fd9387f8bfe4911d6c46c
kingura <kopets@i.ua>
2018-08-10 03:29:12 +0300

    added RectangleLinearGradientBrush, TriangleGradientBrush, StarGradientBrush, BouncingGradientBrushBall

---
+++ TriangleGradientBrush/TriangleGradientBrush.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+class TriangleGradientBrush : PrintableForm
+{
+  public new static void Main()
+  {
+    Application.Run(new TriangleGradientBrush());
+  }
+
+  public TriangleGradientBrush()
+  {
+    Text = "Triangle Gradient Brush";
+  }
+
+  protected override void DoPage(Graphics grfx, Color clr, int cx, int cy)
+  {
+    Point[] apt = { new Point(cx, 0),
+                    new Point(cx, cy),
+                    new Point(0, cy) };
+
+    PathGradientBrush pgbrush = new PathGradientBrush(apt);
+
+    pgbrush.SurroundColors = new Color[] {Color.Red, Color.Green, Color.Blue};
+    grfx.FillRectangle(pgbrush, 0, 0, cx, cy);
+  }
+}
