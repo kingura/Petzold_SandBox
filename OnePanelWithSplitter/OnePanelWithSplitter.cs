0f80480719b6652eba8c84d7a3ac94717cd81d16
kingura <igorkpi@ukr.net>
2019-12-12 01:26:04 +0200

    added OnePanelWithSplitter, TwoPanelsWithSplitter, SplitThreeAcross, SplitThreeFrames, SplitTwoProportional

---
+++ OnePanelWithSplitter/OnePanelWithSplitter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+class OnePanelWithSplitter : Form
+{
+  public static void Main()
+  {
+    Application.Run(new OnePanelWithSplitter());
+  }
+
+  public OnePanelWithSplitter()
+  {
+    Text = "One Panel with Splitter";
+
+    Splitter splitter = new Splitter();
+    splitter.Parent = this;
+    splitter.Dock = DockStyle.Left;
+
+    Panel panel = new Panel();
+    panel.Parent = this;
+    panel.Dock = DockStyle.Left;
+    panel.BackColor = Color.Lime;
+    panel.Resize += new EventHandler(PanelOnResize);
+    panel.Paint += new PaintEventHandler(PanelOnPaint);
+  }
+
+  void PanelOnResize(object obj, EventArgs ea)
+  {
+    ((Panel) obj).Invalidate();
+  }
+
+  void PanelOnPaint(object obj, PaintEventArgs pea)
+  {
+    Panel panel = (Panel) obj;
+    Graphics grfx = pea.Graphics;
+
+    grfx.DrawEllipse(Pens.Black, 0, 0, panel.Width - 1, panel.Height - 1);
+
+  }
+}
